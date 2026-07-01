1eace4bb392add1cea9aa67a27a778d00a759007
Gunanihil N <gunanihil3@gmail.com>
2026-07-01 11:13:03 +0530

    added exercise 1 in week 3

---
+++ week 3/exercise 1/src/test/java/org/example/AppTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest
+{
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
