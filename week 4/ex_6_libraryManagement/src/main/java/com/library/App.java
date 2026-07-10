94384b7f46a075f27dc94d748f10130ced7a9350
Gunanihil N <gunanihil3@gmail.com>
2026-07-10 11:01:27 +0530

    added component scan in ex 6

---
+++ week 4/ex_6_libraryManagement/src/main/java/com/library/App.java
@@ -0,0 +1,18 @@
+package com.library;
+
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+//        Exercise 6 - component scan
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService service = context.getBean("bookService", BookService.class);
+        service.getData();
+        service.saveData();
+    }
+}
---
+++ week 4/ex_6_libraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,25 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    BookRepository repo;
+
+    public BookService(BookRepository repo) {
+        this.repo = repo;
+    }
+
+    public void getData() {
+        System.out.println("Fetched data: " + repo.getData());
+    }
+
+    public void saveData() {
+        System.out.println("Data saved: " + repo.saveData("Raw data"));
+    }
+
+}
